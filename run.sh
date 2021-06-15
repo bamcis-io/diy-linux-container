8c1f71403d443e0d48d692655194a15c040ea0d4
Michael Haken <michael.haken@outlook.com>
2021-06-15 13:47:41 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -71,6 +71,9 @@ environment_cleanup() {
     sudo systemctl stop docker &> /dev/null
     sudo ip netns delete "netns1" || echo ""
     sudo ip link delete hnetns1 || echo ""
+    sudo iptables -D FORWARD -o eth0 -i hnetns1 -j ACCEPT || echo ""
+    sudo iptables -D FORWARD -i eth0 -o hnetns1 -j ACCEPT || echo ""
+    sudo iptables -t nat -D POSTROUTING -s 192.168.100.0/24 -o eth0 -j MASQUERADE || echo ""
     exit
 }
 
